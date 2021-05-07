9370178fad1d49a8cb1b64e601e2557e88c0f0b1
Itamar <itamar@hi4u.me>
2021-05-07 12:54:54 -0300

    modalPlanta

--- assets/Java/script.js
+++ assets/Java/script.js
@@ -6,15 +6,15 @@ for (const carrossel of carrosseis) {
   const prev = carrossel.querySelector(".prev");
   const next = carrossel.querySelector(".next");
   const slider = carrossel.querySelector(".slider");
-  const indicator = carrossel.querySelector(".indicator");
+  const indicator = carrossel.querySelector(".indicator .roll");
   let index = 0;
 
   slider.addEventListener("click", function () {
-	controle.classList.add("active");
+    controle.classList.add("active");
   });
 
   modal.addEventListener("click", function () {
-	controle.classList.remove("active");
+    controle.classList.remove("active");
   });
 
   prev.addEventListener("click", function () {
@@ -57,7 +57,7 @@ for (const carrossel of carrosseis) {
   function circleIndicator() {
     for (let i = 0; i < slider.children.length; i++) {
       const div = document.createElement("div");
-      div.innerHTML = `<img id="${i}" src="${imagens[i]}" class="imgem_pequena" />`;
+      div.innerHTML = `<img id="${i}" src="${imagens[i]}" />`;
       div.addEventListener("click", function () {
         indicateSlide(this);
       });
@@ -102,4 +102,19 @@ for (const carrossel of carrosseis) {
   }
 
   let timer = setInterval(autoPlay, 4000);
+}
+
+const imgPlantas = document.querySelectorAll(".imgPlantas .imagem");
+
+for (const imgPlanta of imgPlantas) {
+  const modalPlanta = imgPlanta.querySelector(".modalPlanta");
+
+  imgPlanta.addEventListener("click", function () {
+    this.classList.add("active");
+  });
+
+  modalPlanta.addEventListener("click", function () {
+    modalPlanta.classList.remove("active");
+    console.log(modalPlanta.element)
+  });
 }
